ab6b364ef3cfebfd50dde76503d9defa9b6837c4
josephenoch <enochjoseph01@gmail.com>
2022-06-03 00:46:06 +0100

    :sparkles: added new routes for the project

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse("<h1> Blog Home </h1>")
+
+
+def about(request):
+    return HttpResponse("<h1>About us</h1>")
 
 # Create your views here.
