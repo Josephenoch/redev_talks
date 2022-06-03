1b78e0855f456490376f1a820693e11afd14af18
josephenoch <enochjoseph01@gmail.com>
2022-06-03 10:15:28 +0100

    :lipstick: add template pages and use template inheritance

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Joseph Enoch',
+        'title': 'Getting started with Django',
+        'content': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil dignissimos eum vero numquam optio aspernatur illum, blanditiis quas quaerat, a mollitia necessitatibus temporibus obcaecati soluta labore voluptate eveniet doloribus odio?',
+        'date': "June 3 2022"
+    },
+    {
+        'author': 'Joseph Enoch',
+        'title': 'Becoming an Advanced React Dev',
+        'content': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil dignissimos eum vero numquam optio aspernatur illum, blanditiis quas quaerat, a mollitia necessitatibus temporibus obcaecati soluta labore voluptate eveniet doloribus odio?',
+        'date': "June 30 2022"
+    }
+]
 
 
 def home(request):
-    return HttpResponse("<h1> Blog Home </h1>")
+    context = {
+        'posts': posts
+    }
+    return render(request, "blog/home.html", context)
 
 
 def about(request):
-    return HttpResponse("<h1>About us</h1>")
+    return render(request, "blog/about.html", {'title': 'About'})
 
 # Create your views here.
