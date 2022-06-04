7e4839329ec2e63e258edaf2f81166a951c9f1ea
josephenoch <enochjoseph01@gmail.com>
2022-06-04 12:16:01 +0100

    :lipstick: styled the jinja template pages

--- blog/views.py
+++ blog/views.py
@@ -5,13 +5,13 @@ posts = [
         'author': 'Joseph Enoch',
         'title': 'Getting started with Django',
         'content': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil dignissimos eum vero numquam optio aspernatur illum, blanditiis quas quaerat, a mollitia necessitatibus temporibus obcaecati soluta labore voluptate eveniet doloribus odio?',
-        'date': "June 3 2022"
+        'date_posted': "June 3 2022"
     },
     {
         'author': 'Joseph Enoch',
         'title': 'Becoming an Advanced React Dev',
         'content': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil dignissimos eum vero numquam optio aspernatur illum, blanditiis quas quaerat, a mollitia necessitatibus temporibus obcaecati soluta labore voluptate eveniet doloribus odio?',
-        'date': "June 30 2022"
+        'date_posted': "June 30 2022"
     }
 ]
 
