ac2df71df0b3ee669435b249bd31355b0895d59b
josephenoch <enochjoseph01@gmail.com>
2022-06-07 16:04:18 +0100

    :card_file_box: create the post db and add dummy data

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Joseph Enoch',
-        'title': 'Getting started with Django',
-        'content': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil dignissimos eum vero numquam optio aspernatur illum, blanditiis quas quaerat, a mollitia necessitatibus temporibus obcaecati soluta labore voluptate eveniet doloribus odio?',
-        'date_posted': "June 3 2022"
-    },
-    {
-        'author': 'Joseph Enoch',
-        'title': 'Becoming an Advanced React Dev',
-        'content': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil dignissimos eum vero numquam optio aspernatur illum, blanditiis quas quaerat, a mollitia necessitatibus temporibus obcaecati soluta labore voluptate eveniet doloribus odio?',
-        'date_posted': "June 30 2022"
-    }
-]
+from blog.models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, "blog/home.html", context)
 
